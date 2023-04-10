9200cd4825b3bc5dd3e4d516d582a94feed007f3
Sriram <xsreeramx@gmail.com>
2023-04-09 20:54:15 -0500

    Few changes

--- Core/accounts/urls.py
+++ Core/accounts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
     path('login/', views.login, name='login'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
